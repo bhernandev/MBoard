8c88ac2274806c79f6c60295c5bc7a8fee0e50c9
Brian Hernandez <Brian@Hernandez-cs.com>
2015-05-24 16:14:37 -0400

    Added all txt files, and created header and documentation (skeleton)

---
+++ board.cpp
@@ -0,0 +1,49 @@
+#include "board.h"
+
+User::User(string u, string p, int i, userType t){
+ id = i;
+ username = u;
+ password = p;
+ type = t;
+ load();
+}
+
+void User::postMessage(string message){
+  
+}
+
+void User::deleteMessage(int i){
+
+}
+
+void User::viewAll(){
+
+}
+
+void User::postAd(string message, string link){
+
+}
+
+void User::deleteAd(int i){
+
+}
+
+void User::viewAds(){
+
+}
+
+void User::allUsers(){
+
+}
+
+void User::messagesBy(string username){
+
+}
+
+void User::deleteUser(string username){
+
+}
+
+void User::load(){
+
+}
---
+++ board.h
@@ -0,0 +1,43 @@
+#ifndef MBOARD
+#define MBOARD
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum userType{
+ admin,
+ business,
+ general
+};
+
+class User{
+ public:
+  //Constructor
+  User(string u, string p, int i, userType t);
+  //General User Methods
+  void postMessage(string message);
+  void deleteMessage(int i);
+  void viewAll();
+  //Business Methods
+  void postAd(string message, string link);
+  void deleteAd(int i);
+  void viewAds();
+  //Admin Methods
+  void allUsers();
+  void messagesBy(string username);
+  void deleteUser(string username);
+ private:
+  int id;
+  string username;
+  string password;
+  userType type;
+  vector<int> mIndexes;
+  //Loads messages or advertisements
+  void load();
+};
+
+#endif
